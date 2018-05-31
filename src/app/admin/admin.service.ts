f35f09165b56ffe8bbdb78683f67e8042d4f6233
Sarun Intaralawan <sarun.intaralawan@gmail.com>
2018-05-31 17:04:46 +0700

    Fix Firebase v5 breaking change

--- src/app/admin/admin.service.ts
+++ src/app/admin/admin.service.ts
@@ -1,12 +1,22 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class AdminService {
   data: Observable<any>;
   // TODO: find uses of this thing
   constructor(private afd: AngularFireDatabase) {
-    this.data = this.afd.list('data').valueChanges();
+    this.data = this.afd.list('data').snapshotChanges().pipe(
+      map(snaps => {
+        return snaps.map(snap => {
+          return {
+            $key: snap.key,
+            ...snap.payload.val()
+          }
+        })
+      })
+    );
   }
 }
